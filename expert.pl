2e1846bee569fa3c2491cdc1c8334f6d514145df
johnhshapiro <jshapir4@msudenver.edu>
2019-11-23 13:46:19 -0700

    added remaining uses

--- expert.pl
+++ expert.pl
@@ -1,92 +1,67 @@
-language(actionscript3.0)  :- is_true('has strong  typing'), is_true('type declaration: implicit with optional explicit typing '), is_true('type checking: static ').
-language(ada)  :- is_true('has strong[TS 1]  typing'), is_true('type declaration: explicit '), is_true('type checking: static ').
-language(aldor)  :- is_true('has weak  typing'), is_true('type declaration: implicit '), is_true('type checking: static ').
-language(algol58)  :- is_true('has strong  typing'), is_true('type declaration: explicit '), is_true('type checking: static ').
-language(algol60)  :- is_true('has strong  typing'), is_true('type declaration: explicit '), is_true('type checking: static ').
-language(algol68)  :- is_true('has strong  typing'), is_true('type declaration: explicit '), is_true('type checking: static & tagged unions ').
-language(apl)  :- is_true('has strong  typing'), is_true('type declaration: '), is_true('type checking: dynamic ').
-language(atejipx)  :- is_true('has strong  typing'), is_true('type declaration: explicit '), is_true('type checking: static ').
-language(basic)  :- is_true('has strong  typing'), is_true('type declaration: explicit '), is_true('type checking: static ').
-language(beanshell)  :- is_true('has strong  typing'), is_true('type declaration: '), is_true('type checking: dynamic ').
-language(boo)  :- is_true('has strong  typing'), is_true('type declaration: implicit with optional explicit typing '), is_true('type checking: static with optional dynamic typing ').
-language(bro)  :- is_true('has strong  typing'), is_true('type declaration: implicit with optional explicit typing '), is_true('type checking: static ').
-language(c)  :- is_true('has weak  typing'), is_true('type declaration: explicit '), is_true('type checking: static ').
-language(cplusplus)  :- is_true('has weak  typing'), is_true('type declaration: explicit with optional implicit typing (by using auto in C++11) '), is_true('type checking: static[TS 2] ').
-language(csharp)  :- is_true('has weak[TS 3]  typing'), is_true('type declaration: implicit with optional explicit typing '), is_true('type checking: static[TS 4] ').
-language(clean)  :- is_true('has strong  typing'), is_true('type declaration: implicit '), is_true('type checking: static ').
-language(clojure)  :- is_true('has strong  typing'), is_true('type declaration: implicit with optional explicit typing '), is_true('type checking: dynamic ').
-language(cobol)  :- is_true('has strong  typing'), is_true('type declaration: explicit '), is_true('type checking: static ').
-language(coldfusion(cfml))  :- is_true('has strong  typing'), is_true('type declaration: implicit '), is_true('type checking: dynamic ').
-language(commonlisp)  :- is_true('has strong  typing'), is_true('type declaration: implicit with optional explicit typing '), is_true('type checking: dynamic, some static checking(depending on implementation) ').
-language(curl)  :- is_true('has strong  typing'), is_true('type declaration: '), is_true('type checking: ').
-language(cython)  :- is_true('has strong  typing'), is_true('type declaration: implicit with optional explicit typing '), is_true('type checking: dynamic with optional static typing ').
-language(d)  :- is_true('has weak[TS 3]  typing'), is_true('type declaration: explicit '), is_true('type checking: static ').
-language(dylan)  :- is_true('has strong  typing'), is_true('type declaration: '), is_true('type checking: dynamic ').
-language(eiffel)  :- is_true('has strong  typing'), is_true('type declaration: '), is_true('type checking: static ').
-language(elixir)  :- is_true('has strong  typing'), is_true('type declaration: implicit '), is_true('type checking: dynamic ').
-language(erlang)  :- is_true('has strong  typing'), is_true('type declaration: implicit '), is_true('type checking: dynamic ').
-language(euphoria)  :- is_true('has strong  typing'), is_true('type declaration: explicit, implicit with objects '), is_true('type checking: static, dynamic with objects ').
-language(fsharp)  :- is_true('has strong  typing'), is_true('type declaration: implicit '), is_true('type checking: static ').
-language(forth)  :- is_true('has typeless  typing'), is_true('type declaration: n/a '), is_true('type checking: n/a ').
-language(fortran)  :- is_true('has strong  typing'), is_true('type declaration: explicit[TS 5] '), is_true('type checking: static ').
-language(gambas)  :- is_true('has strong  typing'), is_true('type declaration: explicit '), is_true('type checking: ').
-language(glbasic)  :- is_true('has strong  typing'), is_true('type declaration: explicit. Non-explicit declarations available through project options '), is_true('type checking: static ').
-language(go)  :- is_true('has strong  typing'), is_true('type declaration: implicit with optional explicit typing '), is_true('type checking: static ').
-language(gosu)  :- is_true('has strong  typing'), is_true('type declaration: partially implicit (local type inference) '), is_true('type checking: static ').
-language(groovy)  :- is_true('has strong  typing'), is_true('type declaration: implicit with optional explicit typing '), is_true('type checking: dynamic with optional static typing ').
-language(harbour)  :- is_true('has strong  typing'), is_true('type declaration: implicit with optional explicit typing '), is_true('type checking: dynamic ').
-language(haskell)  :- is_true('has strong  typing'), is_true('type declaration: implicit with optional explicit typing '), is_true('type checking: static ').
-language(haxe)  :- is_true('has strong  typing'), is_true('type declaration: implicit with optional explicit typing '), is_true('type checking: static with optional dynamic typing ').
-language(io)  :- is_true('has strong  typing'), is_true('type declaration: implicit '), is_true('type checking: dynamic ').
-language(islisp)  :- is_true('has strong  typing'), is_true('type declaration: '), is_true('type checking: dynamic ').
-language(j)  :- is_true('has strong  typing'), is_true('type declaration: '), is_true('type checking: dynamic ').
-language(java)  :- is_true('has strong[2]  typing'), is_true('type declaration: explicit '), is_true('type checking: static ').
-language(javascript)  :- is_true('has weak  typing'), is_true('type declaration: implicit '), is_true('type checking: dynamic ').
-language(julia)  :- is_true('has strong  typing'), is_true('type declaration: implicit with optional explicit typing[3] '), is_true('type checking: dynamic ').
-language(joy)  :- is_true('has strong  typing'), is_true('type declaration: '), is_true('type checking: dynamic ').
-language(kotlin)  :- is_true('has strong  typing'), is_true('type declaration: partially implicit (local type inference) '), is_true('type checking: static ').
-language(labview)  :- is_true('has strong  typing'), is_true('type declaration: '), is_true('type checking: ').
-language(lua)  :- is_true('has strong  typing'), is_true('type declaration: implicit '), is_true('type checking: dynamic ').
-language(maple)  :- is_true('has strong  typing'), is_true('type declaration: '), is_true('type checking: dynamic ').
-language(mathematica)  :- is_true('has strong  typing'), is_true('type declaration: '), is_true('type checking: dynamic ').
-language(matlabm-code)  :- is_true('has strong  typing'), is_true('type declaration: '), is_true('type checking: dynamic ').
-language(modula-2)  :- is_true('has weak[TS 3]  typing'), is_true('type declaration: explicit '), is_true('type checking: static ').
-language(modula-3)  :- is_true('has weak[TS 3]  typing'), is_true('type declaration: explicit '), is_true('type checking: static ').
-language(oberon)  :- is_true('has strong  typing'), is_true('type declaration: explicit '), is_true('type checking: static and partially dynamic[TS 6] ').
-language(objective-c)  :- is_true('has strong  typing'), is_true('type declaration: explicit '), is_true('type checking: dynamic with optional static typing[4] ').
-language(ocaml)  :- is_true('has strong  typing'), is_true('type declaration: implicit with optional explicit typing '), is_true('type checking: static ').
-language(objectpascal)  :- is_true('has strong  typing'), is_true('type declaration: explicit '), is_true('type checking: static ').
-language(opa)  :- is_true('has strong  typing'), is_true('type declaration: implicit with optional explicit typing '), is_true('type checking: static ').
-language(oxygene)  :- is_true('has weak  typing'), is_true('type declaration: implicit '), is_true('type checking: static ').
-language(oz-mozart)  :- is_true('has strong  typing'), is_true('type declaration: implicit '), is_true('type checking: dynamic ').
-language(pascal)  :- is_true('has weak[TS 3]  typing'), is_true('type declaration: explicit '), is_true('type checking: static ').
-language(perl5)  :- is_true('has  typing'), is_true('type declaration: implicit '), is_true('type checking: dynamic ').
-language(php)  :- is_true('has  typing'), is_true('type declaration: implicit with optional explicit typing '), is_true('type checking: dynamic ').
-language(plus)  :- is_true('has strong  typing'), is_true('type declaration: explicit '), is_true('type checking: static, dynamic (optional) ').
-language(prolog)  :- is_true('has  typing'), is_true('type declaration: '), is_true('type checking: dynamic ').
-language(pure)  :- is_true('has  typing'), is_true('type declaration: '), is_true('type checking: dynamic ').
-language(python)  :- is_true('has strong  typing'), is_true('type declaration: implicit (with optional explicit typing as of 3.5) '), is_true('type checking: dynamic ').
-language(raku)  :- is_true('has  typing'), is_true('type declaration: partially implicit[TS 7] '), is_true('type checking: dynamic with optional static typing ').
-language(rebol)  :- is_true('has strong  typing'), is_true('type declaration: implicit '), is_true('type checking: dynamic ').
-language(rpg)  :- is_true('has weak  typing'), is_true('type declaration: '), is_true('type checking: static ').
-language(ruby)  :- is_true('has strong  typing'), is_true('type declaration: implicit '), is_true('type checking: dynamic ').
-language(rust)  :- is_true('has strong  typing'), is_true('type declaration: implicit with optional explicit typing '), is_true('type checking: static ').
-language(s)  :- is_true('has  typing'), is_true('type declaration: '), is_true('type checking: dynamic ').
-language(s-lang)  :- is_true('has strong  typing'), is_true('type declaration: implicit '), is_true('type checking: dynamic ').
-language(scala)  :- is_true('has strong  typing'), is_true('type declaration: partially implicit (local type inference) '), is_true('type checking: static ').
-language(scheme)  :- is_true('has strong  typing'), is_true('type declaration: implicit '), is_true('type checking: dynamic (latent) ').
-language(seed7)  :- is_true('has strong  typing'), is_true('type declaration: explicit '), is_true('type checking: static ').
-language(simula)  :- is_true('has strong  typing'), is_true('type declaration: '), is_true('type checking: static[TS 8] ').
-language(smalltalk)  :- is_true('has strong  typing'), is_true('type declaration: implicit '), is_true('type checking: dynamic ').
-language(swift)  :- is_true('has strong  typing'), is_true('type declaration: partially implicit (local type inference) '), is_true('type checking: static ').
-language(standardml)  :- is_true('has strong  typing'), is_true('type declaration: implicit with optional explicit typing '), is_true('type checking: static ').
-language(tcl)  :- is_true('has  typing'), is_true('type declaration: '), is_true('type checking: dynamic ').
-language(typescript)  :- is_true('has ?  typing'), is_true('type declaration: optional '), is_true('type checking: static ').
-language(visualbasic)  :- is_true('has strong  typing'), is_true('type declaration: implicit with optional explicit typing '), is_true('type checking: static ').
-language(visualbasic.net)  :- is_true('has weak[TS 3]  typing'), is_true('type declaration: explicit '), is_true('type checking: static ').
-language(visualprolog)  :- is_true('has strong  typing'), is_true('type declaration: partially implicit '), is_true('type checking: static ').
-language(wolframlanguage)  :- is_true('has strong  typing'), is_true('type declaration: '), is_true('type checking: dynamic ').
-language(windowspowershell)  :- is_true('has strong  typing'), is_true('type declaration: implicit '), is_true('type checking: dynamic ').
-language(xl)  :- is_true('has strong  typing'), is_true('type declaration: '), is_true('type checking: static ').
-language(xojo)  :- is_true('has strong  typing'), is_true('type declaration: explicit '), is_true('type checking: static ').
-language(xpath/xquery)  :- is_true('has strong  typing'), is_true('type declaration: partially implicit '), is_true('type checking: dynamic with optional static typing ').
+begin :- write("Working on a project? Let me help you choose a programming language!"),
+    askUse,
+    askParadigms,
+    askTyping,
+    askGarbageCollection,
+    guess,
+    retractall.a
+
+askUse :-
+    write("Is it used in: ")
+    web_design(Q) :-
+        format("~w?\n", [Q]),
+        Answer = read(yes),
+        (
+            assert(web_design(Q))
+            assert(web_design)
+        ),
+    applications(Q) :-
+        format("~w?\n", [Q]),
+        Answer = read(yes),
+        (
+            assert(applications(Q))
+            assert(applications)
+        ),
+    system(Q) :-
+        format("~w?\n", [Q]),
+        Answer = read(yes),
+        (
+            assert(applications(Q))
+            assert(applications)
+        ),
+
+    financial(Q) :-
+        format("~w?\n", [Q]),
+        Answer = read(yes),
+        (
+            assert(applications(Q))
+            assert(applications)
+        ),
+        education(Q) :-
+            format("~w?\n", [Q]),
+            Answer = read(yes),
+            (
+                assert(applications(Q))
+                assert(applications)
+            ),
+        mathematics(Q) :-
+            format("~w?\n", [Q]),
+            Answer = read(yes),
+            (
+                assert(applications(Q))
+                assert(applications)
+            ),
+        scripting(Q) :-
+            format("~w?\n", [Q]),
+            Answer = read(yes),
+            (
+                assert(applications(Q))
+                assert(applications)
+            ),
+        linguistics(Q) :-
+            format("~w?\n", [Q]),
+            Answer = read(yes),
+            (
+                assert(applications(Q))
+                assert(applications)
+            ).
--- knowledge_builder.py
+++ knowledge_builder.py
@@ -1,8 +1,9 @@
 import csv
+
 with open('languages.csv', 'r') as data:
     reader = csv.reader(data)
     languages = list(reader)
-prolog_output = open('expert.pl', 'w')
+prolog_output = open('knowledge_base.pl', 'w')
 
 for language in languages:
     name = language[0].lower().replace(' ', '')
@@ -10,3 +11,6 @@ for language in languages:
     type_expression = language[2]
     type_checking = language[4]
     prolog_output.write(f"language({name})  :- is_true('has {type_safety} typing'), is_true('type declaration: {type_expression}'), is_true('type checking: {type_checking}').\n")
+
+
+prolog_output.close()
